8ce72c8a0db1e9c9caf21f68a89a5073a99d2f8e
pluto7073 <20planetpluto05@gmail.com>
2024-12-22 18:36:26 -0500

    MODID basically

--- src/main/java/ml/pluto7073/bartending/foundations/mixin/ArgumentTypesMixin.java
+++ src/main/java/ml/pluto7073/bartending/foundations/mixin/ArgumentTypesMixin.java
@@ -17,12 +17,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class ArgumentTypesMixin {
 
     @Shadow
-    private static <A extends ArgumentType<?>, T extends ArgumentTypeInfo.Template<A>> ArgumentTypeInfo<A, T> register(Registry<ArgumentTypeInfo<?, ?>> registry, String id, Class<? extends A> argumentClass, ArgumentTypeInfo<A, T> info) {
+    public static <A extends ArgumentType<?>, T extends ArgumentTypeInfo.Template<A>> ArgumentTypeInfo<A, T> register(Registry<ArgumentTypeInfo<?, ?>> registry, String id, Class<? extends A> argumentClass, ArgumentTypeInfo<A, T> info) {
         return null;
     }
 
     @Inject(at = @At("RETURN"), method = "bootstrap(Lnet/minecraft/core/Registry;)Lnet/minecraft/commands/synchronization/ArgumentTypeInfo;")
-    private static void enchanted_forest$RegisterCustomArgumentTypes(Registry<ArgumentTypeInfo<?, ?>> registry, CallbackInfoReturnable<ArgumentTypeInfo<?, ?>> cir) {
+    private static void bartending$RegisterCustomArgumentTypes(Registry<ArgumentTypeInfo<?, ?>> registry, CallbackInfoReturnable<ArgumentTypeInfo<?, ?>> cir) {
         register(registry, TheArtOfBartending.MOD_ID + ":alcohol", AlcoholArgument.class, SingletonArgumentInfo.contextAware(AlcoholArgument::new));
     }
 
